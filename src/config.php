00a78ff7c21ed792d9c38695f8b0d177bbe28a98
Tramb-dev <83473808+Tramb-dev@users.noreply.github.com>
2021-05-26 15:23:49 +0200

    Create config.php

---
+++ src/config.php
@@ -0,0 +1,17 @@
+<?php
+/**************************************************************/
+/* config.php
+/* Fichier de configuration (BDD, etc).
+/**************************************************************/
+
+define('SQL_DB', 'xxx');
+define('SQL_NAME', 'xxx');
+define('SQL_LOGIN', 'xxx');
+define('SQL_PASS', 'xxx');
+define('SQL_PORT', 'xxx');
+define('ROOT', 'xxx');
+
+setlocale (LC_TIME, 'fr_FR','fra');
+date_default_timezone_set("Europe/Paris");
+mb_internal_encoding("UTF-8");
+?>
